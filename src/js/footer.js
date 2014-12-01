45aaa78eec149133f402a0ab6a76a049e6d68ee9
Paat Sinsuwan <paat@sinsuwan.com>
2014-12-01 13:57:25 -0500

    Fix scroll top problem

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('#ibm-cookie-preferences-link, #ibm-close-cookie-overlay').on('click', function() {
+  $('#ibm-cookie-preferences-link, #ibm-close-cookie-overlay').on('click', function(e) {
+    e.preventDefault();
     $('#ibm-cloud-cookie-overlay').toggleClass('open');
   });
 });
